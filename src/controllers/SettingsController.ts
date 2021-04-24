5c2c14f0e113588be6cba872c5c05472b9c5bd8d
Adan Bueno <adanbueno73@gmail.com>
2021-04-24 14:05:11 -0300

    feat: Create bd and services this messages

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,21 +1,21 @@
 import { Request, Response } from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "../repositories/SettingsRepository";
+import { SettingsServices } from "../services/SettingsServices";
 
 class SettingsController {
   async create(request: Request, response: Response) {
     const { chat, username } = request.body;
 
-    const settingsRepository = getCustomRepository(SettingsRepository);
+    const settingsService = new SettingsServices();
+    try {
+      const settings = await settingsService.create({ chat, username });
 
-    const settings = settingsRepository.create({
-      chat,
-      username,
-    });
-
-    await settingsRepository.save(settings);
-    return response.json(settings);
+      return response.json(settings);
+    } catch (err) {
+      return response.status(400).json({
+        message: err.message,
+      });
+    }
   }
 }
 
-export { SettingsController}
+export { SettingsController };
